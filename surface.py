e54b85c5cb1e5915e49ea6dfa7bd5f3890426f58
Paula <paisamares12@gmail.com>
2026-07-06 15:33:43 -0500

    Segunda Clase

--- surface.py
+++ surface.py
@@ -13,6 +13,16 @@ height = 600
 surface = pygame.display.set_mode((width, height)) #surface
 #Le damos un título a la ventana del juego
 pygame.display.set_caption("Crabby")   
+"""
+-RGB (Red, Green, Blue) para definir el color de fondo de la ventana del juego.
+-Los colores se definen con valores del 0 al 255, donde 0 es la ausencia del color y 255 
+es el color en su máxima intensidad.
+-Los colores se definen mediante tuplas de 3 valores, donde el primer valor es el rojo, 
+el segundo es el verde y el tercero es el azul.
+"""
+red = pygame.Color(255, 0, 0) #0 - 255
+green = pygame.Color(0, 255, 0) #0 - 255
+blue = pygame.Color(0, 0, 255) #0 - 255
 
 #Generamos un ciclo infinito para que la ventana del juego se mantenga abierta
 while True:
@@ -22,3 +32,5 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit() #Evita errores del sistema al cerrar la ventana del juego
+    surface.fill(blue) #Rellenamos la ventana del juego con el color definido
+    pygame.display.update() #Actualizamos la ventana del juego para que se vea el color de fondo
